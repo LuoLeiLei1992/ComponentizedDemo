bca61ba8b370816928bfe4dae41967774c9af263
ray <ray.luo@immotor.com>
2019-09-27 11:21:37 +0800

    + 初始化

---
+++ ComponentizedDemo/ComponentizedDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ComponentizedDemo
+//
+//  Created by ray on 2019/9/27.
+//  Copyright © 2019 ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ComponentizedDemo/ComponentizedDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ComponentizedDemo
+//
+//  Created by ray on 2019/9/27.
+//  Copyright © 2019 ray. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
